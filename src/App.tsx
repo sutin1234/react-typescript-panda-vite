7b2f1631684ade58210d966118d8f1bb5d4f29aa
Sutin <poly.dev2018@gmail.com>
2023-09-04 14:43:17 +0700

    fix:  SystemStyleObject type style check

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from '@assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { ButtonComponent } from '@components/test.compont'
 import { css } from '@panda/css'
@@ -21,12 +19,6 @@ function App() {
     <>
       <div className={containerStyle}>
         <ButtonComponent title={title} onClick={() => setTitle('New Title Component')} />
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className='card'>
--- src/components/test.compont.tsx
+++ src/components/test.compont.tsx
@@ -1,23 +1,24 @@
 import { css } from '@panda/css'
 import { MouseEventHandler } from 'react'
+import { SystemStyleObject } from '@panda/types'
 
 type ButtonProps = {
   title?: string
   onClick?: MouseEventHandler<HTMLButtonElement>
 }
-const titleStyle = css({
+const buttonStyle: SystemStyleObject = {
   color: 'blue.400',
   fontWeight: 'bold',
   bgColor: 'blue.100',
   px: 8,
   borderRadius: '5px',
-})
+}
 
 export function ButtonComponent(prop: ButtonProps) {
   return (
     <div>
       <div>{prop.title}</div>
-      <button className={titleStyle} onClick={prop.onClick}>
+      <button className={css(buttonStyle)} onClick={prop.onClick}>
         onClick
       </button>
     </div>
