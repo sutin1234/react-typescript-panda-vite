5707d454f36e224c90e512befe611fe4f7c4790d
Sutin <poly.dev2018@gmail.com>
2023-09-04 11:21:20 +0700

    fix: type check pops

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@panda': path.resolve(__dirname, 'node_modules/styled-system'),
+      '@pandaType': path.resolve(__dirname, 'node_modules/styled-system/types'),
       '@components': path.resolve(__dirname, './src/components'),
     }
   }
