19f2f67db96f6038a0bf74f2834025402b4d02ce
Sutin <poly.dev2018@gmail.com>
2023-09-04 12:28:03 +0700

    fix: style lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,40 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  plugins: [
+    'react-refresh',
+    'react',
+    '@typescript-eslint',
+    'react-hooks',
+    'prettier',
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off',
+    'camelcase': 'error',
+    'spaced-comment': 'error',
+    'quotes': ['error', 'single'],
+    'no-duplicate-imports': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      'typescript': {},
+    },
   },
 }
